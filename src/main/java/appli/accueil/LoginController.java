14a3461147feb69f6683e635667691082b5ff4ce
Xenoy28 <titis.ng.ethan@gmail.com>
2026-04-01 18:38:43 +0200

    Update LoginController.java

--- src/main/java/appli/accueil/LoginController.java
+++ src/main/java/appli/accueil/LoginController.java
@@ -1,57 +1,39 @@
 package appli.accueil;
+
 import appli.StartApplication;
-import javafx.event.ActionEvent;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 import java.io.IOException;
 
 public class LoginController {
-
     @FXML
     private TextField emailField;
-
     @FXML
-    private Label labelAurevoir;
-
+    private PasswordField passwordField;
     @FXML
-    private Label labelErreur;
-
+    private Label errorLabel;
+    protected static String email_valide = "mathis@zzz.com";
+    protected String mdp_valide = "mathisZZZ";
     @FXML
-    private PasswordField mdpFeild;
-
-    @FXML
-    void connexion(ActionEvent event) {
+    protected void onConnexionClick() {
         String email = emailField.getText();
-        String password = mdpFeild.getText();
-        System.out.println("email :" + email);
-        System.out.println("password :" + password);
-        if (email.isEmpty() || password.isEmpty()) {
-            labelErreur.setText("Veuillez remplir tous les champs obligatoire ");
-        } else if (email.equals("kamilyoubi27@gmail.com") && password.equals("Azerty1234")) {
-            labelErreur.setText("Vous etes connecte");
-        }else{
-            labelErreur.setText(" les informations fournis ne sont pas complete");
+        String motDePasse = passwordField.getText();
+        System.out.println("Tentative de connexion : " + email);
+        if (email.equals(email_valide) && motDePasse.equals(mdp_valide)) {
+            System.out.println("Connexion réussie !");
+            errorLabel.setText("");
+        } else {
+            errorLabel.setText("Email ou mot de passe incorrect.");
         }
     }
-
-
     @FXML
-    void incription(ActionEvent event) throws IOException {
+    protected void onInscriptionClick() throws IOException {
         StartApplication.changeScene("accueil/Inscription");
-
     }
-
     @FXML
-    void mdpOublie(ActionEvent event) {
-
+    protected void onMotDePasseOublieClick() {
+        System.out.println("Mot de passe oublié cliqué");
     }
-
 }
